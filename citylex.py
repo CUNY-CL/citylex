a7bcfa3d8ae20ab33ea1136936d6119ec1bea0e8
kylebgorman <kylebgorman@gmail.com>
2019-08-08 15:51:25 -0400

    Adds downloading support!

--- citylex.py
+++ citylex.py
@@ -1,36 +1,42 @@
 #!/usr/bin/env python
 """Creates textproto CityLex lexicon."""
 
-## TODO: Add CELEX morphology.
-## TODO: Add downloading support.
-
 import argparse
 import csv
+import io
 import logging
 import re
-import string
+import zipfile
 
-from typing import List
+from typing import Iterator, List
 
 from google.protobuf import text_format
 
 import pandas
+import requests
 
 import citylex_pb2
 
-## Paths to data resource and the associated licenses.
+## Paths to data resources that have to be local.
+# TODO(kbg): Make these into flags.
 
-CELEX_FREQ = "data/celex2/english/efw/efw.cd"
-CELEX_MORPH_LEMMA = "data/celex2/english/eml/eml.cd"
-CELEX_PRON = "data/celex2/english/epw/epw.cd"
-CMU_PRON = "data/cmudict-0.7b"
-ELP = "data/ELP.csv"
-SUBTLEX_UK = "data/SUBTLEX-UK.xlsx"
-SUBTLEX_US = (
-    "data/SUBTLEX-US frequency list with PoS information text version.txt"
+CELEX_FREQ_PATH = "data/celex2/english/efw/efw.cd"
+CELEX_MORPH_LEMMA_PATH = "data/celex2/english/eml/eml.cd"
+CELEX_PRON_PATH = "data/celex2/english/epw/epw.cd"
+ELP_PATH = "data/ELP.csv"
+
+## URLs to automatically downloadable data.
+CMU_PRON_URL = (
+    "http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b"
 )
-UDLEXICONS = "data/UDLexicons.0.2/UDLex_English-Apertium.conllul"
-UNIMORPH = "data/eng"
+SUBTLEX_UK_URL = "http://crr.ugent.be/papers/SUBTLEX-UK.xlsx"
+SUBTLEX_US_URL = "http://crr.ugent.be/papers/SUBTLEX-US_frequency_list_with_PoS_information_final_text_version.zip"
+SUBTLEX_US_PATH = (
+    "SUBTLEX-US frequency list with PoS information text version.txt"
+)
+UNIMORPH_URL = "http://github.com/unimorph/eng/raw/master/eng"
+UDLEXICONS_URL = "http://atoll.inria.fr/~sagot/UDLexicons.0.2.zip"
+UDLEXICONS_PATH = "UDLexicons.0.2/UDLex_English-Apertium.conllul"
 
 ## Fieldnames.
 
@@ -43,6 +49,28 @@ MORPHENTRY_FIELDNAMES = frozenset(["udlexicons", "unimorph"])
 
 def _parse_celex_row(line: str) -> List[str]:
     return line.rstrip().split("\\")
+
+
+def _request_url_resource(url: str) -> Iterator[str]:
+    """Requests a URL and returns text."""
+    logging.info("Requesting URL: %s", url)
+    response = requests.get(url, stream=True)
+    response.raise_for_status()
+    for line in response.iter_lines():
+        yield line.decode("utf8", "ignore")
+
+
+def _request_url_zip_resource(url: str, path: str) -> Iterator[str]:
+    """Requests a zip file by URL and the path to the desired file."""
+    logging.info("Requesting URL: %s", url)
+    response = requests.get(url)
+    response.raise_for_status()
+    # Pretends to be a local zip file.
+    mock_zip_file = io.BytesIO(response.content)
+    # Opens the ZIP file, and then the specific enclosed file.
+    with zipfile.ZipFile(mock_zip_file, "r").open(path, "r") as source:
+        for line in source:
+            yield line.decode("utf8")
 
 
 def main(args: argparse.Namespace) -> None:
@@ -77,7 +105,7 @@ def main(args: argparse.Namespace) -> None:
     if args.enable_celex:
         # CELEX/COBUILD frequencies.
         counter = 0
-        with open(CELEX_FREQ, "r") as source:
+        with open(CELEX_FREQ_PATH, "r") as source:
             for line in source:
                 row = _parse_celex_row(line)
                 wordform = row[1].casefold()
@@ -91,17 +119,11 @@ def main(args: argparse.Namespace) -> None:
                 counter += 1
         logging.info(f"Collected {counter:,} CELEX frequencies")
 
-        """
-        # CELEX wordform morphology.
-        with open(CELEX_MORPH_LEMMA, "r") as source:
-          for line in source:
-              row = _parse_celex_row(line)
-              print(row)
-        """
+        # TODO: Add CELEX morphology.
 
         # CELEX pronunciations.
         counter = 0
-        with open(CELEX_PRON, "r") as source:
+        with open(CELEX_PRON_PATH, "r") as source:
             for line in source:
                 row = _parse_celex_row(line)
                 wordform = row[1].casefold()
@@ -120,24 +142,22 @@ def main(args: argparse.Namespace) -> None:
     if args.enable_cmu:
         # CMU pronunciations.
         counter = 0
-        # There is exactly one misencoded line, which we "ignore".
-        with open(CMU_PRON, "r", errors="ignore") as source:
-            for line in source:
-                if line.startswith(";"):
-                    continue
-                (wordform, pron) = line.rstrip().split("  ", 1)
-                wordform = wordform.casefold()
-                # Removes "numbering" on wordforms like `BASS(1)`.
-                wordform = re.sub(r"\(\d+\)$", "", wordform)
-                ptr = lexicon.entry[wordform]
-                ptr.cmu_pron.append(pron)
-                counter += 1
+        for line in _request_url_resource(CMU_PRON_URL):
+            if line.startswith(";"):
+                continue
+            (wordform, pron) = line.rstrip().split("  ", 1)
+            wordform = wordform.casefold()
+            # Removes "numbering" on wordforms like `BASS(1)`.
+            wordform = re.sub(r"\(\d+\)$", "", wordform)
+            ptr = lexicon.entry[wordform]
+            ptr.cmu_pron.append(pron)
+            counter += 1
         logging.info(f"Collected {counter:,} CMU pronunciations")
 
     if args.enable_elp:
         # ELP morphology.
         counter = 0
-        with open(ELP, "r") as source:
+        with open(ELP_PATH, "r") as source:
             for drow in csv.DictReader(source):
                 wordform = drow["Word"].casefold()
                 ptr = lexicon.entry[wordform]
@@ -154,7 +174,8 @@ def main(args: argparse.Namespace) -> None:
 
         # SUBTLEX-UK frequencies.
         counter = 0
-        with pandas.ExcelFile(SUBTLEX_UK) as source:
+        # Pandas can magically grab URLs.
+        with pandas.ExcelFile(SUBTLEX_UK_URL) as source:
             sheet = source.sheet_names[0]
             # Disables parsing "nan" as, well, `nan`.
             df = source.parse(sheet, na_values=[], keep_default_na=False)
@@ -169,48 +190,50 @@ def main(args: argparse.Namespace) -> None:
 
         # SUBTLEX-US frequencies.
         counter = 0
-        with open(SUBTLEX_US, "r") as source:
-            for drow in csv.DictReader(source, delimiter="\t"):
-                wordform = drow["Word"].casefold()
-                ptr = lexicon.entry[wordform]
-                ptr.subtlex_us_freq = int(drow["FREQcount"])
-                ptr.subtlex_us_cd = int(drow["CDcount"])
-                counter += 1
+        source = _request_url_zip_resource(SUBTLEX_US_URL, SUBTLEX_US_PATH)
+        for drow in csv.DictReader(source, delimiter="\t"):
+            wordform = drow["Word"].casefold()
+            ptr = lexicon.entry[wordform]
+            ptr.subtlex_us_freq = int(drow["FREQcount"])
+            ptr.subtlex_us_cd = int(drow["CDcount"])
+            counter += 1
         logging.info(f"Collected {counter:,} SUBTLEX-US frequencies")
 
     if args.enable_udlexicons:
         # UDLexicon morphology.
-        # We chose not to use the EnLex data here, which looks quite messy in
-        # comparison; maybe revisit this decision someday.
+        # TODO(kbg): We chose not to use the EnLex data here, which looks quite
+        # messy by comparison; maybe revisit this decision someday.
         counter = 0
-        with open(UDLEXICONS, "r") as source:
-            for line in source:
-                line = line.rstrip()
-                pieces = line.split("\t")
-                # Skips complex expressions.
-                # TODO(kbg): Is there a more elegant way to do this?
-                if pieces[0].startswith("0-"):
-                    continue
-                wordform = pieces[2].casefold()
-                ptr = lexicon.entry[wordform]
-                lemma = pieces[3].casefold()
-                entry = ptr.udlexicons.add()
-                # Ignores unspecified lemmata.
-                if lemma != "_":
-                    entry.lemma = lemma
-                entry.pos = pieces[4]
-                features = pieces[6]
-                # Ignores unspecified feature bundles.
-                if features != "_":
-                    entry.features = features
-                counter += 1
+        source = _request_url_zip_resource(UDLEXICONS_URL, UDLEXICONS_PATH)
+        for line in source:
+            pieces = line.rstrip().split("\t")
+            # Skips complex expressions.
+            # TODO(kbg): Is there a more elegant way to do this?
+            if pieces[0].startswith("0-"):
+                continue
+            wordform = pieces[2].casefold()
+            ptr = lexicon.entry[wordform]
+            lemma = pieces[3].casefold()
+            entry = ptr.udlexicons.add()
+            # Ignores unspecified lemmata.
+            if lemma != "_":
+                entry.lemma = lemma
+            entry.pos = pieces[4]
+            features = pieces[6]
+            # Ignores unspecified feature bundles.
+            if features != "_":
+                entry.features = features
+            counter += 1
         logging.info(f"Collected {counter:,} UDLexicon analyses")
 
-    # Unimorph.
-    counter = 0
-    with open(UNIMORPH, "r") as source:
-        for line in source:
-            (lemma, wordform, features) = line.rstrip().split("\t", 2)
+    if args.enable_unimorph:
+        # Unimorph.
+        counter = 0
+        for line in _request_url_resource(UNIMORPH_URL):
+            line = line.rstrip()
+            if not line:
+                continue
+            (lemma, wordform, features) = line.split("\t", 2)
             wordform = wordform.casefold()
             lemma = lemma.casefold()
             ptr = lexicon.entry[wordform]
@@ -218,14 +241,14 @@ def main(args: argparse.Namespace) -> None:
             entry.lemma = lemma
             entry.features = features
             counter += 1
-    logging.info(f"Collected {counter:,} UniMorph analyses")
+        logging.info(f"Collected {counter:,} UniMorph analyses")
 
-    logging.info("Writing out tables...")
-    # Writes it out as a textproto.
+    logging.info("Writing out textproto")
     with open(args.output_textproto_path, "w") as sink:
         text_format.PrintMessage(lexicon, sink, as_utf8=True)
         logging.debug("Wrote %d entries", len(lexicon.entry))
-    # Writes it out as a TSV file.
+
+    logging.info("Writing out TSV")
     with open(args.output_tsv_path, "w") as sink:
         tsv_writer = csv.DictWriter(
             sink,
@@ -237,22 +260,23 @@ def main(args: argparse.Namespace) -> None:
         tsv_writer.writeheader()
         # Sorting for stability.
         for (wordform, entry) in sorted(lexicon.entry.items()):
-            row = {"wordform": wordform}
+            drow = {"wordform": wordform}
             for field in fieldnames:
                 if field in REPEATED_STRING_FIELDNAMES:
                     # Deduplicate and join on '^'.
-                    row[field] = "^".join(frozenset(getattr(entry, field)))
+                    drow[field] = "^".join(frozenset(getattr(entry, field)))
                 elif field in MORPHENTRY_FIELDNAMES:
                     # Make triples and join on '^'.
                     triples = [
                         f"{triple.lemma}_{triple.pos}_{triple.features}"
                         for triple in getattr(entry, field)
                     ]
-                    row[field] = "^".join(triples)
+                    drow[field] = "^".join(triples)
                 elif entry.HasField(field):
-                    row[field] = getattr(entry, field)
-            tsv_writer.writerow(row)
-    logging.info("...done")
+                    drow[field] = getattr(entry, field)
+            tsv_writer.writerow(drow)
+
+    logging.info("Success!")
 
 
 if __name__ == "__main__":
--- data/README.md
+++ data/README.md
@@ -6,42 +6,4 @@ include:
 
     tar -xzf LDC96L14.tgz
 
-* CMU Pronouncing Dictionary [BSD-2-Clause]: This resource is obtained from
-
-    http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b
-
-* MorphoLex-en (commit hash b3884b9) [CC-BY]: This resource is obtained from
-    
-    https://github.com/hugomailhot/MorphoLex-en/raw/master/MorphoLEX_en.xlsx
-
-* SUBTLEX-UK [CC-BY-NC-ND]: This resource is obtained directly from
-
-    http://crr.ugent.be/papers/SUBTLEX-UK.xlsx
-
-* SUBTLEX-US [CC-BY-NC-ND]: This resource is obtained directly from
-
-    http://crr.ugent.be/papers/SUBTLEX-US_frequency_list_with_PoS_information_final_text_version.zip
-
-The file mus tbe decompressed using
-
-    unzip -x SUBTLEX-US_frequency_list_with_PoS_information_final_text_version.zip
-
-* UDLexicons 0.2 Apertium [GPL]: This resource is obtained from
-
-    http://atoll.inria.fr/~sagot/UDLexicons.0.2.zip
-
-The file must be decompressed using
-
-    unzip UDLexicons.0.2.zip UDLexicons.0.2/UDLex_English-Apertium.conllul
-
-* UDLexicons 0.2 EnLex [LGPL-LR]: This resource is obtained from
-
-    http://atoll.inria.fr/~sagot/UDLexicons.0.2.zip
-
-The file must be decompressed using
-
-    unzip UDLexicons.0.2.zip UDLexicons.0.2/UDLex_English-EnLex.conllul
-
-* UniMorph (commit hash bbd8c13) [CC-BY-SA]: This resource is obtained from
-
-    https://github.com/unimorph/eng/raw/master/eng
+* ELP [noncommercial]: FIXME
