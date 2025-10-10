f3642e67843056202af6f5ab06c604af18c439b6
Kyle Gorman <kylebgorman@gmail.com>
2025-10-10 18:04:28 -0400

    Many many cleanups to feature table.
    * I gave up making the feature table a list of tuples and went to a full
    dictionary format where every thing I want to express is clearly
    expressed.
    * Tests updated to reflect.
    * Webapp interface updated to reflect.
    * Some minor clarifications in the index template: "UniMorph
      (UniMorph-style tags)" is now written just "UniMorph features", since
    they are what they are.

--- citylex/features.py
+++ citylex/features.py
@@ -3,235 +3,228 @@
 * For CELEX, see the CELEX English manual (ch. 3).
 * For UniMorph, see https://unimorph.github.io/doc/unimorph-schema.pdf.
 * For Universal Dependencies, see https://universaldependencies.org/u/feat/.
-
-Use `tag_to_tag` to retrieve actual mappings.
-
-NOTE ON NULLABLE/DEFEASIBLE MAPPINGS:
-- In the mapping tuples below, an empty string "" indicates a deliberate
-  exclusion for UD tags that have no distinct equivalent in that system or are
-  systematically syncretic. Callers should SKIP emitting entries when
-  `tag_to_tag()` returns "".
 """
 
-from typing import Dict, Optional
-
-# The format is (CELEX tag, UniMorph tag, UD tag). Use an interior list to
-# indicate that multiple tags in one system map to a single tag in another;
-# in this case, only the first tag will be mapped to.
-_map_cols = ("CELEX", "UniMorph", "UD")
-_map_tuples = [
-    # Adverbs.
-    ("B", "ADV", "ADV"),
-    ("B", "ADV", "ADV|_"),
-    ("", "ADV;CMPR", "ADV|Degree=Cmp"),
-    ("", "ADV;SPRL", "ADV|Degree=Sup"),
-    # Adjectives.
-    ("b", "ADJ", "ADJ"),
-    ("b", "ADJ", "ADJ|_"),
-    ("c", "ADJ;CMPR", "ADJ|Degree=Cmp"),
-    ("s", "ADJ;SPRL", "ADJ|Degree=Sup"),
-    # Verbs.
-    # CELEX collapses imperative/infinitive/present to 'eP'.
-    # UniMorph has no entry due to imperative-subjunctive syncretism.
-    ("eP", "", "VERB|Mood=Imp"),
-    ("eP", "V;NFIN;IMP+SBJV", "VERB|VerbForm=Inf"),
-    ("eP", "V;PRS", "VERB|Tense=Pres"),
-    # CELEX 'pe' for present participle (canonical -ing).
-    ("pe", "V;V.PTCP;PRS", "VERB|Tense=Pres|VerbForm=Part"),
-    # Gerund is syncretic with the -ing participle.
-    ("", "V;GER", "VERB|VerbForm=Ger"),
-    # Past participle.
-    ("pa", "V;V.PTCP;PST", "VERB|Tense=Past|VerbForm=Part"),
-    # Simple past.
-    ("a1S", "V;PST", "VERB|Tense=Past"),
-    # UD verb person/number are syntactic only (except 3rd sg present).
-    ("", "", "VERB|Number=Sing|Person=1|Tense=Pres"),
-    ("", "", "VERB|Number=Sing|Person=2|Tense=Pres"),
-    # Only morphologically-realized person/number form in English.
-    ("e3S", "V;PRS;3;SG", "VERB|Number=Sing|Person=3|Tense=Pres"),
-    ("", "", "VERB|Number=Plur|Person=1|Tense=Pres"),
-    ("", "", "VERB|Number=Plur|Person=2|Tense=Pres"),
-    ("", "", "VERB|Number=Plur|Person=3|Tense=Pres"),
-    ("", "", "VERB|Number=Sing|Person=1|Tense=Past"),
-    ("", "", "VERB|Number=Sing|Person=2|Tense=Past"),
-    ("", "", "VERB|Number=Sing|Person=3|Tense=Past"),
-    ("", "", "VERB|Number=Plur|Person=1|Tense=Past"),
-    ("", "", "VERB|Number=Plur|Person=2|Tense=Past"),
-    ("", "", "VERB|Number=Plur|Person=3|Tense=Past"),
-    ("", "V;SBJV;PRS", "VERB|Mood=Sub|Tense=Pres"),
-    # Nouns.
-    (
-        "S",
-        "N;SG",
-        [
-            "NOUN|Number=Sing",
-            "PROPN|Number=Sing",
-            # CELEX doesn't track gender for English;
-            # UD marks gender on proper nouns referring to people.
-            "PROPN|Gender=Fem|Number=Sing",
-            "PROPN|Gender=Masc|Number=Sing",
-        ],
-    ),
-    (
-        "P",
-        "N;PL",
-        [
-            "NOUN|Number=Plur",
-            "PROPN|Number=Plur",
-            "PROPN|Gender=Fem|Number=Plur",
-            "PROPN|Gender=Masc|Number=Plur",
-        ],
-    ),
-    # Bare nouns don't appear in UniMorph English.
-    ("", "", "NOUN"),
-    ("", "", "NOUN|_"),
-    ("", "", "PROPN"),
-    ("", "", "PROPN|_"),
-    # Closed classes without morphological features.
-    ("", "", "DET"),
-    ("", "", "DET|_"),
-    ("", "DET;SG", "DET|Number=Sing"),
-    ("", "DET;PL", "DET|Number=Plur"),
-    # Explicit Definite forms.
-    ("", "", "DET|Definite=Ind"),
-    ("", "DET;SG", "DET|Definite=Ind|Number=Sing"),
-    ("", "DET;PL", "DET|Definite=Ind|Number=Plur"),
-    ("", "DET;PL", "DET|Number=Plur|Person=3"),
-    # Pronouns.
-    ("", "", "PRON"),
-    # Enumerated singular forms.
-    (
-        "",
-        "PRON;SG",
-        [
-            "PRON|Number=Sing",
-            "PRON|Number=Sing|Person=1",
-            "PRON|Number=Sing|Person=2",
-            "PRON|Number=Sing|Person=3",
-            # Gender distinctions exist in UD/UM.
-            "PRON|Gender=Masc|Number=Sing",
-            "PRON|Gender=Fem|Number=Sing",
-            "PRON|Gender=Neut|Number=Sing",
-            "PRON|Gender=Masc|Number=Sing|Person=3",
-            "PRON|Gender=Fem|Number=Sing|Person=3",
-            "PRON|Gender=Neut|Number=Sing|Person=3",
-            # Features sometimes attached in UD.
-            "PRON|Definite=Ind|Number=Sing",
-            "PRON|Reflex=Yes|Number=Sing",
-            "PRON|Poss=Yes|Number=Sing",
-            "PRON|Case=Nom|Number=Sing",
-            "PRON|Case=Acc|Number=Sing",
-        ],
-    ),
-    (
-        "",
-        "PRON;PL",
-        [
-            "PRON|Number=Plur",
-            "PRON|Number=Plur|Person=1",
-            "PRON|Number=Plur|Person=2",
-            "PRON|Number=Plur|Person=3",
-            "PRON|Gender=Masc|Number=Plur",
-            "PRON|Gender=Fem|Number=Plur",
-            "PRON|Gender=Neut|Number=Plur",
-            "PRON|Gender=Masc|Number=Plur|Person=2",
-            "PRON|Gender=Fem|Number=Plur|Person=1",
-            "PRON|Definite=Ind|Number=Plur",
-            "PRON|Reflex=Yes|Number=Plur",
-            "PRON|Poss=Yes|Number=Plur",
-        ],
-    ),
-    # Pronouns without Number don't appear in UniMorph English.
-    ("", "", "PRON|PronType=Rel"),
-    ("", "", "PRON|Gender=Neut"),
-    ("", "", "PRON|Person=1"),
-    ("", "", "PRON|Person=2"),
-    ("", "", "PRON|Person=3"),
-    ("", "", "PRON|Gender=Masc|Person=2"),
-    ("", "", "PRON|Gender=Fem|Person=2"),
-    ("", "", "PRON|Poss=Yes"),
-    ("", "", "PRON|Reflex=Yes"),
-    ("", "", "PRON|Definite=Ind"),
-    ("", "", "PRON|Case=Nom"),
-    ("", "", "PRON|Case=Acc"),
-    # Numerals.
-    ("", "", "NUM"),
-    ("", "", "NUM|_"),
-    ("", "NUM;PL", "NUM|Number=Plur"),
-    ("", "NUM;SG", "NUM|Number=Sing"),
-    # Closed classes.
-    ("", "", "ADP"),
-    ("", "", "ADP|_"),
-    ("", "", "SCONJ"),
-    ("", "", "SCONJ|_"),
-    ("", "", "PART"),
-    ("", "", "PART|_"),
-    ("", "", "INTJ"),
-    ("", "", "INTJ|_"),
-    ("", "", "SYM"),
-    ("", "", "SYM|_"),
-    ("", "", "X"),
-    ("", "", "X|_"),
-    ("", "", "AUX"),
-    ("", "", "AUX|_"),
-    ("", "", "CCONJ"),
-    ("", "", "CCONJ|_"),
-    # Auxiliaries with features.
-    ("", "", "AUX|VerbForm=Inf"),
-    ("", "", "AUX|VerbForm=Ger"),
-    ("", "", "AUX|Tense=Pres"),
-    ("", "", "AUX|Tense=Past"),
-    ("", "", "AUX|Tense=Past|VerbForm=Part"),
-    ("", "", "PUNCT"),
-    ("", "", "PUNCT|_"),
-    ("", "", "PART|Polarity=Neg"),
-]
+from typing import Dict, List, Union
 
 
-def _inner_dict_factory(from_index: int, to_index: int) -> Dict[str, str]:
-    result = {}
-    for row in _map_tuples:
-        from_tag = row[from_index]
-        to_tag = row[to_index]
-        if isinstance(from_tag, str):
-            if isinstance(to_tag, str):
-                result[from_tag] = to_tag
-            else:
-                # Only taking the first tag from the interior list.
-                result[from_tag] = to_tag[0]
-        else:
-            assert isinstance(to_tag, str), "unexpected many-to-many mapping"
-            for tag in from_tag:
-                result[tag] = to_tag
-    return result
-
-
-# Turns the above into dictionaries. The format is
-# [source tag][destination tag][tag].
-_map_dict = {
-    outer: {
-        inner: _inner_dict_factory(i, j)
-        for j, inner in enumerate(_map_cols)
-        if i != j
-    }
-    for i, outer in enumerate(_map_cols)
+# Format is:
+# [source tag][from_tag][destination_tag] = to_tag or [to_tag, to_tag, ...]
+# tag_to_tag uses a different order, but this is much easier to define.
+_map_dict: Dict[str, Dict[str, Dict[str, Union[str, List[str]]]]] = {
+    "CELEX": {
+        # Adverb.
+        "B": {"UniMorph": "ADV", "UD": "ADV"},
+        # Adjective.
+        "b": {"UniMorph": "ADJ", "UD": "ADJ"},
+        "c": {"UniMorph": "ADJ;CMPR", "UD": "ADJ|Degree=Cmp"},
+        "s": {"UniMorph": "ADJ;SPRL", "UD": "ADJ|Degree=Sup"},
+        # Noun.
+        "S": {"UniMorph": "N;SG"},  # Can't say whether it's proper in UD.
+        "P": {"UniMorph": "N;PL"},  # Ditto.
+        # Verb.
+        "i": {"UniMorph": "V;NFIN;IMP+SBJV", "UD": "VERB|VerbForm=Inf"},
+        "pe": {
+            "UniMorph": "V;V.PTCP;PRS",
+            "UD": "VERB|Tense=Pres|VerbForm=Part",
+        },
+        "e1S": {
+            "UniMorph": "V;PRS",
+            "UD": "VERB|Number=Sing|Person=1|Tense=Pres",
+        },
+        "e2S": {
+            "UniMorph": "V;PRS",
+            "UD": "VERB|Number=Sing|Person=2|Tense=Pres",
+        },
+        "e3S": {
+            "UniMorph": "V;PRS;3;SG",
+            "UD": "VERB|Number=Sing|Person=3|Tense=Pres",
+        },
+        "eP": {
+            "UniMorph": "V;PRS",
+            "UD": [
+                "VERB|Number=Plur|Person=1|Tense=Pres",
+                "VERB|Number=Plur|Person=2|Tense=Pres",
+                "VERB|Number=Plur|Person=3|Tense=Pres",
+            ],
+        },
+        "a1S": {
+            "UniMorph": "V;PST",
+            "UD": "VERB|Number=Sing|Person=1|Tense=Past",
+        },
+        "a2S": {
+            "UniMorph": "V;PST",
+            "UD": "VERB|Number=Sing|Person=2|Tense=Past",
+        },
+        "a3S": {
+            "UniMorph": "V;PST;3;SG",
+            "UD": "VERB|Number=Sing|Person=3|Tense=Past",
+        },
+        "aP": {
+            "UniMorph": "V;PST",
+            "UD": [
+                "VERB|Number=Plur|Person=1|Tense=Past",
+                "VERB|Number=Plur|Person=2|Tense=Past",
+                "VERB|Number=Plur|Person=3|Tense=Past",
+            ],
+        },
+        "pa": {
+            "UniMorph": "V;V.PTCP;PST",
+            "UD": "VERB|Tense=Past|VerbForm=Part",
+        },
+    },
+    "UniMorph": {
+        # Adverb.
+        "ADV": {"CELEX": "B", "UD": "ADV"},
+        # No comparative adverbs in CELEX.
+        "ADV;CMPR": {"UD": "ADV|Degree=Cmp"},
+        # No superlative adverbs in CELEX.
+        "ADV;SPLR": {"UD": "ADV|Degree=Sup"},
+        # Adjective.
+        "ADJ": {"CELEX": "b", "UD": "ADJ"},
+        "ADJ;CMPR": {"CELEX": "c", "UD": "ADJ|Degree=Cmp"},
+        "ADJ;SPLR": {"CELEX": "s", "UD": "ADJ|Degree=Sup"},
+        # Noun.
+        "N;SG": {"CELEX": "S"},  # Can't say whether it's proper in UD.
+        "N;PL": {"CELEX": "P"},  # Ditto.
+        # Verb.
+        "V;NFIN;IMP+SBJV": {
+            "CELEX": "i",
+            "UD": ["VERB|Mood=Imp", "VERB|VerbForm=Inf"],
+        },
+        "V;V.PTCP;PRS": {"CELEX": "ae", "UD": "VERB|Tense=Pres|VerbForm=Part"},
+        "V;GER": {"UD": "VERB|VerbForm=Ger"},  # No gerunds in CELEX.
+        "V;PRS": {
+            "CELEX": ["e1S", "e2S", "eP"],
+            "UD": [
+                "VERB|Number=Sing|Person=1|Tense=Pres",
+                "VERB|Number=Sing|Person=2|Tense=Pres",
+                "VERB|Number=Plur|Person=1|Tense=Pres",
+                "VERB|Number=Plur|Person=2|Tense=Pres",
+                "VERB|Number=Plur|Person=3|Tense=Pres",
+            ],
+        },
+        "V;PRS;3;SG": {
+            "CELEX": "e3S",
+            "UD": "VERB|Number=Sing|Person=3|Tense=Pres",
+        },
+        # No present subjunctive in CELEX.
+        "V;SBJV;PRS": {"UD": "VERB|Mood=Sub|Tense=Pres"},
+        "V;PST": {
+            "CELEX": ["a1S", "a2S", "a3S", "aP"],
+            "UD": [
+                "VERB|Number=Sing|Person=1|Tense=Past",
+                "VERB|Number=Sing|Person=2|Tense=Past",
+                "VERB|Number=Sing|Person=3|Tense=Past",
+                "VERB|Number=Plur|Person=1|Tense=Past",
+                "VERB|Number=Plur|Person=2|Tense=Past",
+                "VERB|Number=Plur|Person=3|Tense=Past",
+            ],
+        },
+        "V;V.PTCP;PST": {"CELEX": "pa", "UD": "VERB|Tense=Past|VerbForm=Part"},
+    },
+    "UD": {
+        # Adverb.
+        "ADV": {"CELEX": "B", "UniMorph": "ADV"},
+        # No comparative adverbs in CELEX.
+        "ADV|Degree=Cmp": {"UniMorph": "ADV;CMPR"},
+        # No superlative adverbs in CELEX.
+        "ADV|Degree=Sup": {"UniMorph": "ADV;SPRL"},
+        # Adjective.
+        "ADJ": {"CELEX": "b", "UniMorph": "ADJ"},
+        "ADJ|Degree=Cmp": {"CELEX": "c", "UniMorph": "ADJ;CMPR"},
+        "ADJ|Degree=Sup": {"CELEX": "s", "UniMorph": "ADJ;SPRL"},
+        # Noun.
+        "NOUN|Number=Sing": {"CELEX": "S", "UniMorph": "N;SG"},
+        "NOUN|Number=Plur": {"CELEX": "P", "UniMorph": "N;PL"},
+        "PROPN|Gender=Fem|Number=Sing": {"CELEX": "S", "UniMorph": "N;SG"},
+        "PROPN|Gender=Masc|Number=Sing": {"CELEX": "S", "UniMorph": "N;SG"},
+        "PROPN|Number=Plur": {"CELEX": "P", "UniMorph": "N;PL"},
+        "PROPN|Gender=Fem|Number=Plur": {"CELEX": "P", "UniMorph": "N;PL"},
+        "PROPN|Gender=Masc|Number=Plur": {"CELEX": "P", "UniMorph": "N;PL"},
+        # Verb.
+        "VERB|VerbForm=Inf": {"CELEX": "i", "UniMorph": "V;NFIN;IMP+SBJV"},
+        # No imperative in CELEX.
+        "VERB|Mood=Imp": {"UniMorph": "V;NFIN;IMP+SBJV"},
+        "VERB|Tense=Pres|VerbForm=Part": {
+            "CELEX": "pe",
+            "UniMorph": "V;V.PTCP;PRS",
+        },
+        "VERB|VerbForm=Ger": {"UniMorph": "V;GER"},  # No gerunds in CELEX.
+        "VERB|Number=Sing|Person=1|Tense=Pres": {
+            "CELEX": "e1S",
+            "UniMorph": "V;PRS",
+        },
+        "VERB|Number=Sing|Person=2|Tense=Pres": {
+            "CELEX": "e2S",
+            "UniMorph": "V;PRS",
+        },
+        "VERB|Number=Sing|Person=3|Tense=Pres": {
+            "CELEX": "e3S",
+            "UniMorph": "V;PRS;3;SG",
+        },
+        "VERB|Number=Plur|Person=1|Tense=Pres": {
+            "CELEX": "eP",
+            "UniMorph": "V;PRS",
+        },
+        "VERB|Number=Plur|Person=2|Tense=Pres": {
+            "CELEX": "eP",
+            "UniMorph": "V;PRS",
+        },
+        "VERB|Number=Plur|Person=3|Tense=Pres": {
+            "CELEX": "eP",
+            "UniMorph": "V;PRS",
+        },
+        # No present subjunctive in CELEX.
+        "VERB|Mood=Sub|Tense=Pres": {"UniMorph": "V;SBJV;PRS"},
+        "VERB|Number=Sing|Person=1|Tense=Past": {
+            "CELEX": "a1S",
+            "UniMorph": "V;PST",
+        },
+        "VERB|Number=Sing|Person=2|Tense=Past": {
+            "CELEX": "a2S",
+            "UniMorph": "V;PST",
+        },
+        "VERB|Number=Sing|Person=3|Tense=Past": {
+            "CELEX": "a3S",
+            "UniMorph": "V;PST",
+        },
+        "VERB|Number=Plur|Person=1|Tense=Past": {
+            "CELEX": "aP",
+            "UniMorph": "V;PST",
+        },
+        "VERB|Number=Plur|Person=2|Tense=Past": {
+            "CELEX": "aP",
+            "UniMorph": "V;PST",
+        },
+        "VERB|Number=Plur|Person=3|Tense=Past": {
+            "CELEX": "aP",
+            "UniMorph": "V;PST",
+        },
+        "VERB|Tense=Past|VerbForm=Part": {
+            "CELEX": "pa",
+            "UniMorph": "V;V.PTCP;PST",
+        },
+    },
 }
 
 
-def tag_to_tag(from_name: str, to_name: str, tag: str) -> Optional[str]:
+def tag_to_tag(
+    from_name: str, to_name: str, tag: str
+) -> Union[str, List[str], None]:
     """Maps a morphological tag in one feature system to another.
 
     Args:
         from_name: the source system for the morphological tag (one of:
             "CELEX", "UniMorph", "UD").
         to_name: the target for the morphological tag (one of:
-            "CELEX", "UniMorph, "UD").
+            "CELEX", "UniMorph", "UD").
         tag: the source system tag to look up.
 
     Returns:
-        The tag in the target system. If it is empty,
-        the mapping was intentionally excluded for compatibility.
-        If it is None, the requested tag was not found.
+        Tag, list of tags, or None.
     """
-    assert from_name != to_name, "no-op mapping"
-    return _map_dict[from_name][to_name].get(tag)
+    try:
+        return _map_dict[from_name][tag][to_name]
+    except KeyError:
+        return None
